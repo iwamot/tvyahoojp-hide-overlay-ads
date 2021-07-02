34ff0909161da250fcdd60da0cda17f2e32dd71f
Takashi Iwamoto <hello@iwamot.com>
2021-07-02 23:51:50 +0900

    Update script

--- tvyahoojp-hide-overlay-ads.user.js
+++ tvyahoojp-hide-overlay-ads.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Yahoo!テレビ - Hide overlay ads
 // @namespace    http://iwamot.com/
-// @version      1.0.0
+// @version      1.0.1
 // @author       IWAMOTO Takashi <hello@iwamot.com> http://iwamot.com/
 // @description  Yahoo!テレビのオーバーレイ広告を非表示にします。
 // @include      https://tv.yahoo.co.jp/*
 // @homepage     https://github.com/iwamot/tvyahoojp-hide-overlay-ads
 // @updateURL    https://github.com/iwamot/tvyahoojp-hide-overlay-ads/raw/master/tvyahoojp-hide-overlay-ads.user.js
+// @grant        GM_addStyle
 // ==/UserScript==
 
-$("#yadsOverlay").remove();
+GM_addStyle('aside {display: none;}');
